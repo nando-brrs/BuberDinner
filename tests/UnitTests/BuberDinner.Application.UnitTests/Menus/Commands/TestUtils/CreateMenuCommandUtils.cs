d66d1e1e86d6468c98212d3efc7363f01db09cd9
Fernando Barros <nando.brrs@gmail.com>
2023-07-04 17:29:05 -0300

    adding tests

---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
@@ -0,0 +1,34 @@
+using BuberDinner.Application.Menus.Commands;
+using BuberDinner.Application.UnitTests.TestUtils.Constants;
+
+namespace BuberDinner.Application.UnitTests.Menus.Commands.TestUtils;
+
+public class CreateMenuCommandUtils
+{
+    //name
+    //description
+    //list of sections
+    public static CreateMenuCommand CreateCommand(List<MenuSectionCommand>? sections = null)
+        => new CreateMenuCommand(
+        new Guid(Constants.Host.Id.ToString()!),
+        Constants.Menu.Name,
+        Constants.Menu.Description,
+        sections ?? CreateSectionsCommands()
+    );
+
+    public static List<MenuSectionCommand> CreateSectionsCommands(
+        int sectionCount = 1, List<MenuItemCommand>? items = null) =>
+        Enumerable.Range(0, sectionCount)
+            .Select(index => new MenuSectionCommand(
+                Constants.Menu.SectionNameFromGivenIndex(index),
+                Constants.Menu.SectionDescriptionFromGivenIndex(index),
+                CreateItemsCommands()
+            )).ToList();
+
+    public static List<MenuItemCommand> CreateItemsCommands(int itemCount = 1) =>
+        Enumerable.Range(0, itemCount)
+            .Select(index => new MenuItemCommand(
+                Constants.Menu.ItemNameFromGivenIndex(index),
+                Constants.Menu.ItemDescriptionFromGivenIndex(index)
+            )).ToList();
+}
